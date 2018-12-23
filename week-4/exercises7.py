dc482fac4d449db74ca0c34554a85a7453181d50
Abdul-Latif-31 <46087371+Abdul-Latif-31@users.noreply.github.com>
2018-12-24 01:41:10 +0600

    Added Useful Comment
    Few lines have been explained better using some useful comment

--- week-4/exercises7.py
+++ week-4/exercises7.py
@@ -1,5 +1,8 @@
+#Importing sqrt function from math 
 from math import sqrt
+#Capital X and Y removed and used small letter x and y
 x = 2
 y = 4
 print("The product of ", x, "and", y, "is", x * y)
+#Printing sqrt of y-x instead of x-y and making the value positive
 print("The root of their difference is ", sqrt(y - x))
