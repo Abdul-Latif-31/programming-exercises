04a70b67ecb56dc9d399ede83b926a308a2ad507
Abdul-Latif-31 <46087371+Abdul-Latif-31@users.noreply.github.com>
2018-12-24 00:55:11 +0600

    Create exercises1.py

---
+++ week-3/exercises1.py
@@ -0,0 +1,5 @@
+numberOne = int(input("Please enter a number : "))
+numberTwo = int(input("Please enter a number : "))
+sumof2numbers = numberOne + numberTwo
+print("Sum of the numbers is : ")
+print(sumof2numbers)
