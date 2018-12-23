b24d00ea580a36dd6937b66a1916ed327f290e65
Abdul-Latif-31 <46087371+Abdul-Latif-31@users.noreply.github.com>
2018-12-24 01:42:16 +0600

    Added Useful Comment
    Explained why input has been used

--- week-4/exercises8.py
+++ week-4/exercises8.py
@@ -1,3 +1,4 @@
+#Using input to take username and show "Enter your name: " to the user
 username = input("Enter your name : ")
 userage = int(input("Enter your age : "))
 print("Hello {0}, next year you will be {1} years old!".format(username,userage))
