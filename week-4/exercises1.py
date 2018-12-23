e4b74bec6da02edef3b0de031a8b2e1007be06cc
Abdul-Latif-31 <46087371+Abdul-Latif-31@users.noreply.github.com>
2018-12-24 01:32:32 +0600

    Added useful comment
    Indicating what caused problem in the code

--- week-4/exercises1.py
+++ week-4/exercises1.py
@@ -1,5 +1,6 @@
+#Corrected exchanging the position of input and int and this line now saves the input as integer
 radius = int(input("Radius: "))
 x = 3.14
 pi = x
 area = pi * radius ** 2
-print(area)
+print(area) 
